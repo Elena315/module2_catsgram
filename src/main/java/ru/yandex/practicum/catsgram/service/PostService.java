2302bc0fe3036f2951cf6687d0a6c429ebf1d051
Elena <Alcabarena2006@ya.ru>
2023-02-24 09:11:36 +0300

    add endpoint

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.catsgram.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.PostNotFoundException;
 import ru.yandex.practicum.catsgram.exception.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.model.User;
@@ -13,6 +14,7 @@ import java.util.List;
 public class PostService {
     private final UserService userService;
     private final List<Post> posts = new ArrayList<>();
+    private static Integer globalId = 0;
 
     @Autowired
     public PostService(UserService userService){
@@ -23,6 +25,10 @@ public class PostService {
         return posts;
     }
 
+    private static Integer getNextId(){
+        return globalId++;
+    }
+
     public Post create(Post post) {
         User postAuthor = userService.findUserByEmail(post.getAuthor());
         if (postAuthor == null) {
@@ -30,7 +36,15 @@ public class PostService {
                     "Пользователь %s не найден",
                     post.getAuthor()));
         }
+        post.setId(getNextId());
         posts.add(post);
         return post;
+    }
+
+    public Post findPostById(Integer postId) {
+        return posts.stream()
+                .filter(p -> p.getId().equals(postId))
+                .findFirst()
+                .orElseThrow(() -> new PostNotFoundException(String.format("Пост № %d не найден", postId)));
     }
 }
